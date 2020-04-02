d617c1281c2775ad70589521fe12839e251d946d
shafali03 <shaf03sm@gmail.com>
2020-04-02 22:54:48 +0100

    Created edit functionality to todo list item

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Item from "./TodoItem";
+// import Item from "./TodoItem";
 import TodoItem from "./TodoItem";
 export default class TodoList extends Component {
   render() {
