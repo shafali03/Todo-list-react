b97f452cfa245db201899110e5b0ea9aa61d1281
shafali03 <shaf03sm@gmail.com>
2020-04-02 19:09:19 +0100

    feat: Add todo item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { v4 as uuidv4 } from "uuid";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
@@ -26,26 +25,22 @@ class App extends Component {
     };
     const updatedItems = [...this.state.items, newItem];
 
-    this.setState(
-      {
-        items: updatedItems,
-        item: "",
-        id: uuidv4(),
-        editItem: false
-      },
-      () => console.log(this.state)
-    );
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuidv4(),
+      editItem: false
+    });
   };
   clearList = () => {
     console.log("clear list ");
   };
   handleDelete = id => {
-    console.log(`handle edit ${id}`);
+    console.log(`handle delete ${id}`);
   };
   handleEdit = id => {
-    console.log(`edit ${id}`);
+    console.log(`edit edit ${id}`);
   };
-
   render() {
     return (
       <div className="container">
@@ -61,7 +56,7 @@ class App extends Component {
             <TodoList
               items={this.state.items}
               clearList={this.clearList}
-              handleDelete={this.handleChange}
+              handleDelete={this.handleDelete}
               handleEdit={this.handleEdit}
             />
           </div>
